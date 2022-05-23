dbba7d5adb69fff794ab065f7c491ff3f25abf7d
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-05-24 01:00:50 +0200

    feat: add goodbye phrase

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,15 +1,15 @@
 const path = require('path');
 const fs = require('fs').promises;
 
-fs.writeFile( path.join('./text.txt'), '', function(err) {
-    if(err) {
+fs.writeFile(path.join('./text.txt'), '', function (err) {
+    if (err) {
         throw err
     }
-}); 
+});
 
 async function change(loc, dest) {
     try {
-      await fs.rename(loc, dest)
+        await fs.rename(loc, dest)
     } catch (error) {
         throw error
     }
@@ -21,11 +21,26 @@ const readline = require('readline');
 const read = readline.createInterface(process.stdin, process.stdout);
 let reply;
 
-read.question('Введите текст', (words) => {
-    reply += words;
+function reading() {read.question('Введите текст', (words) => {
+    reply = ' ' + words;
+    if (reply === undefined) {
+        reply = ' ';
+    } 
+    else {
     if (reply.toLowerCase().trim() === 'exit') {
         read.close()
     } else {
         fs.writeFile('02-write-file/text.txt', reply);
+        reading();
     }
-});
+    }})
+    return reply;
+};
+
+read.on('close', function() {
+    console.log(' До свидания');
+    process.exit();
+})
+
+reading();
+
